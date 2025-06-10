aea503d1519d855b39f53d5349bf63b149ffdc96
MihaiCatalinVoicu <mihai.catalin_voicu@yahoo.com>
2025-06-10 21:28:58 +0300

    REST CRUD openapi swagger data rest

---
+++ 04-spring-boot-rest-crud/06-openapi-swagger-starter-projects-spring-data-rest/cruddemo/src/main/java/com/dev/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.dev.cruddemo.dao;
+
+import com.dev.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+//@RepositoryRestResource(path = "members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
